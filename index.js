8b4085daaa14ee2ef7d2f5d67cda433a42780e46
Jesse Tane <jesse.tane@gmail.com>
2016-07-11 18:47:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var EventEmitter = require('events')
+
+module.exports = class Collection extends EventEmitter {
+  constructor (opts) {
+    super(this)
+    this.storage = opts.storage
+    this.orderBy = opts.orderBy
+    this.where = opts.where
+    this.items = opts.items || {}
+    if (opts.watch !== false) {
+      this.watch()
+    }
+  }
+
+  watch () {
+    if (this.watching) return
+    this.watching = true
+
+    this._ref = this.storage
+
+    var orderBy = this.orderBy
+    if (orderBy) {
+      this._ref = this._ref
+        .orderByChild(orderBy)
+    }
+
+    var where = this.where
+    if (where) {
+      this._ref = this._ref
+        .equalTo(where)
+    }
+
+    this._ref.on('child_added', this._onchange)
+    this._ref.on('child_changed', this._onchange)
+    this._ref.on('child_removed', this._onremove)
+  }
+
+  unwatch () {
+    if (!this.watching) return
+    this.watching = false
+
+    this._ref.off('child_added', this._onchange)
+    this._ref.off('child_changed', this._onchange)
+    this._ref.off('child_removed', this._onremove)
+  }
+
+  _onchange = (snap, old) => {
+    var key = snap.key
+    var oldData = old ? old.val() : null
+    var newData = snap.val()
+    this.items[key] = newData
+    this.emit('change', {
+      key,
+      oldData,
+      newData,
+    })
+  }
+
+  _onremove = snap => {
+    var key = snap.key
+    var oldData = snap.val()
+    delete this.items[key]
+    this.emit('change', {
+      key,
+      oldData, 
+      newData: null,
+    })
+  }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# realtime-collection
+Collection helper for working with firebase. WIP!
