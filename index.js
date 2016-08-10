cd2795ed929d49307be7922fe1c6bfd909f531e2
Jesse Tane <jesse.tane@gmail.com>
2016-08-09 22:01:31 -0400

    use es5 style classes for compatibility

--- index.js
+++ index.js
@@ -1,81 +1,84 @@
+module.exports = RealtimeCollection
+
+var inherits = require('inherits')
 var EventEmitter = require('events')
 
-module.exports = class extends EventEmitter {
-  constructor (opts) {
-    super()
-    this._onchange = this._onchange.bind(this)
-    this._onremove = this._onremove.bind(this)
-    this._onerror = this._onerror.bind(this)
-    this.parseKey = opts.parseKey || this.parseKey
-    this.storage = opts.storage
-    this.orderBy = opts.orderBy
-    this.where = opts.where
-    this.items = opts.items || {}
-    if (opts.watch !== false) {
-      this.watch()
-    }
+inherits(RealtimeCollection, EventEmitter)
+
+function RealtimeCollection (opts) {
+  EventEmitter.call(this)
+  this._onchange = this._onchange.bind(this)
+  this._onremove = this._onremove.bind(this)
+  this._onerror = this._onerror.bind(this)
+  this.parseKey = opts.parseKey || this.parseKey
+  this.storage = opts.storage
+  this.orderBy = opts.orderBy
+  this.where = opts.where
+  this.items = opts.items || {}
+  if (opts.watch !== false) {
+    this.watch()
+  }
+}
+
+RealtimeCollection.prototype.watch = function () {
+  if (this.watching) return
+  this.watching = true
+
+  this._ref = this.storage
+
+  var orderBy = this.orderBy
+  if (orderBy) {
+    this._ref = this._ref
+      .orderByChild(orderBy)
   }
 
-  watch () {
-    if (this.watching) return
-    this.watching = true
-
-    this._ref = this.storage
-
-    var orderBy = this.orderBy
-    if (orderBy) {
-      this._ref = this._ref
-        .orderByChild(orderBy)
-    }
-
-    var where = this.where
-    if (where) {
-      this._ref = this._ref
-        .equalTo(where)
-    }
-
-    this._ref.on('child_added', this._onchange, this._onerror)
-    this._ref.on('child_changed', this._onchange, this._onerror)
-    this._ref.on('child_removed', this._onremove, this._onerror)
+  var where = this.where
+  if (where) {
+    this._ref = this._ref
+      .equalTo(where)
   }
 
-  unwatch () {
-    if (!this.watching) return
-    this.watching = false
+  this._ref.on('child_added', this._onchange, this._onerror)
+  this._ref.on('child_changed', this._onchange, this._onerror)
+  this._ref.on('child_removed', this._onremove, this._onerror)
+}
 
-    this._ref.off('child_added', this._onchange, this._onerror)
-    this._ref.off('child_changed', this._onchange, this._onerror)
-    this._ref.off('child_removed', this._onremove, this._onerror)
-  }
+RealtimeCollection.prototype.unwatch = function () {
+  if (!this.watching) return
+  this.watching = false
 
-  parseKey (key) {
-    return key
-  }
+  this._ref.off('child_added', this._onchange, this._onerror)
+  this._ref.off('child_changed', this._onchange, this._onerror)
+  this._ref.off('child_removed', this._onremove, this._onerror)
+}
 
-  _onchange (snap) {
-    var key = this.parseKey(snap.key)
-    var oldData = this.items[key]
-    var newData = snap.val()
-    this.items[key] = newData
-    this.emit('change', {
-      key,
-      oldData,
-      newData,
-    })
-  }
+RealtimeCollection.prototype.parseKey = function (key) {
+  return key
+}
 
-  _onremove (snap) {
-    var key = this.parseKey(snap.key)
-    var oldData = snap.val()
-    delete this.items[key]
-    this.emit('change', {
-      key,
-      oldData, 
-      newData: null,
-    })
-  }
+RealtimeCollection.prototype._onchange = function (snap) {
+  var key = this.parseKey(snap.key)
+  var oldData = this.items[key]
+  var newData = snap.val()
+  this.items[key] = newData
+  this.emit('change', {
+    key,
+    oldData,
+    newData,
+  })
+}
 
-  _onerror (err) {
-    this.emit('error', err)
-  }
+RealtimeCollection.prototype._onremove = function (snap) {
+  var key = this.parseKey(snap.key)
+  var oldData = snap.val()
+  delete this.items[key]
+  this.emit('change', {
+    key,
+    oldData,
+    newData: null,
+  })
+}
+
+RealtimeCollection.prototype._onerror = function (err) {
+  this.emit('error', err)
 }
